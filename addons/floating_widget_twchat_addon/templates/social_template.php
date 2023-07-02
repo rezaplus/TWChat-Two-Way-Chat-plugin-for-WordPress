542b30a87d9a8edd6cbf57630b3873abbb6fc3d8
Reza Hajizadeh <33846853+rezaplus@users.noreply.github.com>
2023-07-02 15:49:32 +0300

    Revert "big update"

--- addons/floating_widget_twchat_addon/templates/social_template.php
+++
@@ -1,7 +0,0 @@
-<div class="TWCHSocial">
-    <?php foreach ($social_links as $social) {
-        if ($social->link) {
-            echo '<a href="' . esc_attr($social->link) . '" target="_blank"><img src="' . esc_url($social->icon) . '" alt="' . esc_attr($social->name) . '"></a>';
-        }
-    } ?>
-</div>
